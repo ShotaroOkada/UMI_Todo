cefcf7fc7203b6f0d6749d800b7b164a4bbadd20
OkadaShotaro <b1016117@fun.ac.jp>
2019-06-20 14:52:06 +0900

    [refactor]不要なコメントアウト及びcontainerの削除

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,28 +4,6 @@ import Area from 'src/components/Area';
 import Progress from 'src/components/Progress';
 import IState from 'src/states';
 
-
-// export interface IAppConnectProps {
-//     display: IDisplay
-// }
-
-// export default class App extends React.Component<IAppConnectProps> {
-//     public render() {
-//         return(
-//             <div style={{height: '100%'}}>
-//                 { this.props.display.nowDisplay === 'area' &&
-//                     <Area />
-//                 }
-//                 { this.props.display.nowDisplay === 'progress' &&
-//                     <Progress />
-//                 }
-
-//             </div>
-
-//         )
-//     }
-// }
-
 function App() {
     const nowDisplay = useSelector((state: IState) => state.display.nowDisplay)
     return (
