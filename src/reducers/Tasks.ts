800eca1b1292dedf593311993123324bbf76a82c
ShotaroOkada <b1016117@gmail.com>
2019-11-07 22:38:28 +0900

    localStrageにタスク情報を保存するよう変更

--- src/reducers/Tasks.ts
+++ src/reducers/Tasks.ts
@@ -7,14 +7,15 @@ const getAllTasksToLocalStorage = localStorage.getItem('Tasks');
 const InitialState: ITasks = getAllTasksToLocalStorage ? JSON.parse(getAllTasksToLocalStorage) : initialState;
 
 // localStrageに全てのタスクを保存する関数
-// function setTasksToLocalStrage(state: ITasks) {
-//     localStorage.setItem('Tasks', JSON.stringify(state))
-// }
+function setTasksToLocalStrage(state: ITasks) {
+    localStorage.setItem('Tasks', JSON.stringify(state))
+}
 
 export default function tasks(state: ITasks = InitialState, action: TasksAction): ITasks {
+    let newState: ITasks;
     switch (action.type) {
         case TasksActionType.ADD_TASK:
-            return {
+            newState = {
                 ...state,
                 [action.payload.areaName]: {
                     ...state[action.payload.areaName],
@@ -24,8 +25,10 @@ export default function tasks(state: ITasks = InitialState, action: TasksAction)
                     ]
                 }    
             };
+            setTasksToLocalStrage(newState);
+            return newState;
         case TasksActionType.DELETE_TASK:
-            return {
+            newState = {
                 ...state,
                 [action.payload.areaName]: {
                     ...state[action.payload.areaName],
@@ -34,12 +37,14 @@ export default function tasks(state: ITasks = InitialState, action: TasksAction)
                         ...state[action.payload.areaName][action.payload.progressName].slice(action.payload.taskIndex + 1)
                     ]
                 }
-            }
+            };
+            setTasksToLocalStrage(newState);
+            return newState;
         case TasksActionType.CHANGE_ORDER_TASK:
             const newOrderTasks = [...state[action.payload.areaName][action.payload.progressName]]
             newOrderTasks.splice(action.payload.sourceTaskIndex, 1)
             newOrderTasks.splice(action.payload.destinationTaskIndex, 0, action.payload.taskName)
-            return {
+            newState = {
                 ...state,
                 [action.payload.areaName]: {
                     ...state[action.payload.areaName],
@@ -48,12 +53,14 @@ export default function tasks(state: ITasks = InitialState, action: TasksAction)
                     ]
                 }
             }
+            setTasksToLocalStrage(newState);
+            return newState;
         case TasksActionType.CHANGE_PROGRESS_TASK:
             const newSourceTasks = [...state[action.payload.areaName][action.payload.sourceProgressName]]
             newSourceTasks.splice(action.payload.sourceTaskIndex, 1);
             const newDestinationTasks = [...state[action.payload.areaName][action.payload.destinationProgressName]]
             newDestinationTasks.splice(action.payload.destinationTaskIndex, 0, action.payload.taskName)
-            return {
+            newState = {
                 ...state,
                 [action.payload.areaName]: {
                     ...state[action.payload.areaName],
@@ -65,6 +72,8 @@ export default function tasks(state: ITasks = InitialState, action: TasksAction)
                     ]
                 }
             }
+            setTasksToLocalStrage(newState);
+            return newState;
         default:
             return state;
     }
